e2c664b1fbf9bb2c93c09e7966cab8ad80a47b07
Ricardo <ric.dalvarez@hotmail.com>
2023-07-04 10:44:05 +0200

    specify dbName on connection

--- culdampolla/config.ts
+++ culdampolla/config.ts
@@ -5,8 +5,6 @@ import dotenv from "dotenv";
 dotenv.config({ path: path.resolve(__dirname, "./config.env") });
 
 // Interface to load env variables
-// Note these variables can possibly be undefined
-// as someone could skip these varibales or not setup a .env file at all
 
 interface ENV {
   NODE_ENV: string | undefined;
@@ -29,12 +27,6 @@ const getConfig = (): ENV => {
     MONGO_URI: process.env.MONGO_URI,
   };
 };
-
-// Throwing an Error if any field was undefined we don't
-// want our app to run if it can't connect to DB and ensure
-// that these fields are accessible. If all is good return
-// it as Config which just removes the undefined from our type
-// definition.
 
 const getSanitzedConfig = (config: ENV): Config => {
   for (const [key, value] of Object.entries(config)) {
---
+++ culdampolla/interfaces.ts
@@ -0,0 +1,12 @@
+export interface Product {
+  nombre: string;
+  calle: string;
+  numero: number;
+  piso: number;
+  puerta: string;
+  codigoPostal: number;
+  pais: string;
+  telefono: number;
+  fax: number;
+  nif: string;
+}
