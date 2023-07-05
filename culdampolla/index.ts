e959d5568055f1fd67ca2aab4299c57d8a928860
Ricardo <ric.dalvarez@hotmail.com>
2023-07-05 13:32:28 +0200

    definie whole structure

--- culdampolla/index.ts
+++ culdampolla/index.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import config from "./config";
 import { Provider } from "./interfaces";
-import { MongoClient, Db, Collection } from "mongodb";
+import { MongoClient, Db, Collection, ObjectId } from "mongodb";
 
 const client = new MongoClient(config.MONGO_URI);
 
@@ -10,12 +10,44 @@ async function run(): Promise<void> {
     const culdampolla: Db = client.db("culdampolla");
     const providers: Collection<Provider> = culdampolla.collection("providers");
 
-    const query: Partial<Provider> = { nombre: "test" };
+    const query: Partial<Provider> = { name: "hala" };
     const provider: Provider | null = await providers.findOne(query);
 
     console.log(provider);
+    //time to query 1.5secx
   } finally {
     await client.close();
   }
 }
+//move to controller file
+async function insert(): Promise<void> {
+  try {
+    const culdampolla: Db = client.db("culdampolla");
+    const providers: Collection<Provider> = culdampolla.collection("providers");
+
+    const provider: Provider = {
+      _id: new ObjectId(),
+      name: "hala",
+      address: {
+        street: "granvia",
+        number: "1a",
+        floor: "1",
+        door: "a",
+        city: "barcelona",
+        postalCode: "08041",
+        country: "españa",
+      },
+      phone: "637666665",
+      fax: "test",
+      NIF: "test",
+    };
+
+    const result = await providers.insertOne(provider);
+    console.log(result);
+  } finally {
+    await client.close();
+  }
+}
+
 run().catch(console.dir);
+insert().catch(console.dir);
--- culdampolla/interfaces.ts
+++ culdampolla/interfaces.ts
@@ -1,12 +1,60 @@
+import { ObjectId } from "mongodb";
 export interface Provider {
-  nombre: string;
-  calle: string;
-  numero: number;
-  piso: number;
-  puerta: string;
-  codigoPostal: number;
-  pais: string;
-  telefono: number;
-  fax: number;
-  nif: string;
+  _id: ObjectId;
+  name: String;
+  address: {
+    street: String;
+    number: String;
+    floor: String;
+    door: String;
+    city: String;
+    postalCode: String;
+    country: String;
+  };
+  phone: String;
+  fax: String;
+  NIF: String;
+}
+
+export interface Eyewear {
+  _id: ObjectId;
+  brand: String;
+  provider: {
+    _id: ObjectId;
+    name: String;
+  };
+  prescription: {
+    left: String;
+    right: String;
+  };
+  frameType: String;
+  frameColor: String;
+  lensColor: String;
+  price: Number;
+}
+
+export interface Clients {
+  _id: ObjectId;
+  name: String;
+  address: String;
+  phone: String;
+  email: String;
+  registrationDate: Date;
+  referrer: {
+    _id: ObjectId;
+    name: String;
+  };
+}
+
+export interface Sales {
+  _id: ObjectId;
+  eyewear: {
+    _id: ObjectId;
+    brand: String;
+  };
+  employee: {
+    _id: ObjectId;
+    name: String;
+  };
+  saleDate: Date;
 }
